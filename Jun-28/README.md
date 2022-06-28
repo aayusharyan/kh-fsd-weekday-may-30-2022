315401ac9f641179faaf994fba879c238e43f925
Aayush Sinha <aayush.aryan@me.com>
2022-06-28 20:06:13 +0530

    Jun-28 session

---
+++ Jun-28/README.md
@@ -0,0 +1,26 @@
+# Jun-28, 2022
+
+JS is a dynamic Typed Language || Loosely Typed Language
+
+## typeof operator
+
+## operator
+- It is basically something that operates of the data.
+
+=  -> Assignment Operator
+== -> Equality Comparison Operator
+>  -> Greater than comparison
+>= -> Greater than or equal to. (>= ✔️, => ❌)
+<= -> Less than or equal to
+<  -> Less than.
++  -> Addition.
+
+
+### String concatenation
+- This is the process of combining two string together.
+"Aayush" + " " + "Sinha" -> "Aayush Sinha"
+
+
+### modulo operator
+- The % symbol.
+- 
---
+++ Jun-28/index.js
@@ -0,0 +1,97 @@
+console.log("Hey everyone");
+
+// let variable1 = false;
+// console.log(variable1);
+
+// let number1 = 1337;
+// console.log(number1);
+
+// let string1 = "ABCD";
+// console.log(string1);
+
+// This is in Java (Example of Statically Typed Language)
+// Strictly Typed Language
+// int varsdkkdskh = 14;
+// bool kjkdhsjf = true;
+
+// console.log(typeof variable1);
+
+// console.log(5 > 5);
+// console.log(5 >= 5);
+
+
+// console.log(1 + 2); // Number 1  + Number 2 -> Number 3
+// console.log("1" + "2"); // String 1 + String 2 -> String 12
+// let another = "1" + "2";
+// console.log(typeof another);
+// "100" + "100" => "100100"
+// console.log(1 + "2"); // String 12
+// console.log(1 + 1 + "2"); // String 22
+// console.log(1 + 1 + "1" + 1 + 1); //212, 2111, 41, 
+
+// console.log("3" - "1");
+// console.log( 3  -  1 );
+
+// console.log(2 + 3 + "4" - 10 + 5); //49 as a Number
+
+// let remainder = 123 % 6;
+// let div = 123 / 6;
+
+// console.log(remainder);
+// console.log(div);
+
+
+// Task - Assume a number and check whether it is orr or even.
+
+// let number    = 62;
+// let remainder = number % 2;
+
+// if(remainder == 0) {
+//   console.log(number + " is an even number");
+// } else {
+//   console.log(number + " is an odd number");
+// }
+
+
+// Task 2 - Write logic for FizzBuzz
+// If a number is divisible by 3 and 5 (15), FizzBuzz
+// If a number is divisible by 3, then Fizz
+// If a number is divisible by 5, then Buzz
+// Otherwise, print the number
+// if, else if, else if, else / modulo.
+
+// let number      = 82;
+// let remainder3  = number % 3;
+// let remainder5  = number % 5;
+// let remainder15 = number % 15;
+
+// if(remainder15 == 0) {
+//   console.log("FizzBuzz");
+// } else if (remainder3 == 0) {
+//   console.log("Fizz");
+// } else if (remainder5 == 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log(number);
+// }
+
+//Task - Print numbers from 1 to 10
+
+// let number = 1;
+// while (number < 10) {
+//   console.log(number);
+//   number = number + 1;
+// }
+
+// Task Print number from 20 to 1 (in reverse) 20, 19, 18, 17... 1
+// let number = 20;
+// while (number > 0) {
+//   console.log(number);
+//   number = number - 1;
+// }
+
+// Task - Print all EVEN numbers from 1 to 99. 2, 4, 6, 8, 10.. 98.
+
+
+
+// Task -> Assume a number and check if it is prime or not.
